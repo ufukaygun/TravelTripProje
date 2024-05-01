c08d9f1ac60280cdd0fe460b24a0d4c185e427bb
ufukaygun <u.aygun869@gmail.com>
2024-05-01 14:06:37 +0300

    Hakkımızda ve Rehber sayfası oluşturuldu

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -13,5 +13,9 @@ namespace TravelTripProje.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
