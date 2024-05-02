3f629bb574257321e3d7aefc8af963b50affd2c6
ufukaygun <u.aygun869@gmail.com>
2024-05-02 19:14:29 +0300

    Blog Listeleri için veritabanında data çekildi

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
         }
     }
 }
