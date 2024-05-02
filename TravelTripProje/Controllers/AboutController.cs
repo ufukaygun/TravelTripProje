0dbbaf01f93be7c9ec5f2b7286cc17fceb9e9b5b
ufukaygun <u.aygun869@gmail.com>
2024-05-02 17:50:02 +0300

    Hakkımızda Sayfası verileri listelendi

---
+++ TravelTripProje/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        //Context sınıfından c nesnesi türetildi
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
