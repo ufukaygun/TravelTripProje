59f3acb04902040d471e7bf974411994c3fe872e
ufukaygun <u.aygun869@gmail.com>
2024-05-04 23:54:21 +0300

    IEnumerable ile bir Viewde birden fazla tablo listeleme yapıldı

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -16,10 +16,12 @@ namespace TravelTripProje.Controllers
             var bloglar = c.Blogs.ToList();
             return View(bloglar);
         }
+        BlogYorum by = new BlogYorum();
         public ActionResult BlogDetay(int id)
         {
-            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-            return View(blogbul);
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(by);
         }
     }
 }
---
+++ TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class BlogYorum
+    {
+        //belli sayıdaki değerleri sayı formatında tutar.IEnumerable birden fazla tablodan değer çekilebilir
+        public IEnumerable<Blog> Deger1 { get; set; }
+        public IEnumerable<Yorumlar> Deger2 { get; set; }
+    }
+}
