336d2b8c321eb8f87c516c8d746d4da336dc33d3
ufukaygun <u.aygun869@gmail.com>
2024-05-07 15:47:06 +0300

    Ana Sayfa Slider Görselleri Veritabanından çekildi

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
