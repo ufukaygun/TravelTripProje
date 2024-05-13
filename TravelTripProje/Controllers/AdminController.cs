dc47b732d9e1a57215c20e45d6e63bfb242b6f96
ufukaygun <u.aygun869@gmail.com>
2024-05-13 12:56:49 +0300

    Admin Paneli Veri Tabanında Blog Listesi getirme yapıldı

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        Context c = new Context(); 
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+
+            return View(degerler);
         }
     }
 }
