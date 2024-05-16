2c91c0f1e14204337dead0489c25fffb072b0f51
ufukaygun <u.aygun869@gmail.com>
2024-05-16 19:52:28 +0300

    LogOut işlemi yapıldı

--- TravelTripProje/Controllers/GirisYapController.cs
+++ TravelTripProje/Controllers/GirisYapController.cs
@@ -37,5 +37,10 @@ namespace TravelTripProje.Controllers
                 return View(); 
             }  
         }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "GirisYap");
+        }
     }
 }
